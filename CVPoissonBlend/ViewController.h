c5bdb7193d524c2a76853a44028c4bafce489525
mu1101i <mulloli@me.com>
2013-04-17 12:04:53 +0530

    Initial Commit

---
+++ CVPoissonBlend/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CVPoissonBlend
+//
+//  Created by Varun Mulloli on 17/04/13.
+//  Copyright (c) 2013 Varun Mulloli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CVPoissonBlend/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CVPoissonBlend
+//
+//  Created by Varun Mulloli on 17/04/13.
+//  Copyright (c) 2013 Varun Mulloli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
